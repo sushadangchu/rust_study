1798ed92a7bb27ee5204c9cb76c45bfe736a438f
ldl <746620536@qq.com>
2020-10-20 09:39:58 +0800

    改进minigrep项目,使用迭代器代替之前的for循环和clone代码

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,19 +23,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("arguments not enough");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let case_sensitive = if args.len() == 4 {
-            if args[3] == "CASE_INSENSITIVE=1" {
-                false
-            } else {
-                true
-            }
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+        
+        let case_sensitive_str = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a case_sensitive"),
+        };
+
+        let case_sensitive = if case_sensitive_str == "CASE_INSENSITIVE=1" {
+            false
         } else {
             true
         };
@@ -49,25 +56,29 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    // let mut result = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         result.push(line);
+    //     }
+    // }
+    // result
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result = Vec::new();
+    // let mut result = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         result.push(line);
+    //     }
+    // }
+    // result
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 #[cfg(test)]
